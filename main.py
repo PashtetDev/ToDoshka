2827c89d8321673c8fe3e5f69558347549ca3b30
PashtetDev <pashtetdev@gmail.com>
2024-03-04 17:51:15 +0300

    Import commit (test)

---
+++ main.py
@@ -0,0 +1,4 @@
+#project page: https://my-lost-happiness.notion.site/ToDoshka-c990b671443d47bc99bb3b15eac8b81e
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
